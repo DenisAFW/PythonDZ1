8a0efaa39e0aea2f097041c2d26e1e3281b0d444
Choppa <denis.afinogenow@yandex.ru>
2022-09-05 20:22:11 +0300

    Смена решения

--- DZ2,1.py
+++ DZ2,1.py
@@ -9,23 +9,39 @@
 
 
 # 1.
-x = int(input('Введите число x '))
-y = int(input('Введите число y '))
-z = int(input('Введите число z '))
+# x = int(input('Введите число x '))
+# y = int(input('Введите число y '))
+# z = int(input('Введите число z '))
 
-a = x * y * z
-b = x + y + z
+# a = x * y * z
+# b = x + y + z
 
-if a > 0:
-    a = 0
-elif a < 1:
-    a = 1
-if b > 0:
-    b = 1
-elif b < 1:
-    b = 1
+# if a > 0:
+#     a = 0
+# elif a < 1:
+#     a = 1
+# if b > 0:
+#     b = 1
+# elif b < 1:
+#     b = 1
 
-if a == b:
+# if a == b:
+#     print('Утверждение истинно')
+# elif a != b:
+#     print('Утверждение ложно')
+
+leftSide = not (x or y or z)
+rightSide = not x and not y and not z
+result = leftSide == rightSide
+
+if result == True:
     print('Утверждение истинно')
-elif a != b:
+else:
     print('Утверждение ложно')
+
+
+# Идеальное рещение
+# for x in range(2):
+#         for y in range(2):
+#             for z in range(2):
+#                 print(not (x or y or z) == (not x and not y and not z))
